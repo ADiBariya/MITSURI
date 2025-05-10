13ef076255b1359e0f7457f74508c1bd99aad24d
ADiBariya <adiiibariya@gmail.com>
2025-05-10 15:39:06 +0530

    Update helpers.py

--- Mitsuri/utils/helpers.py
+++ Mitsuri/utils/helpers.py
@@ -1,1 +1,23 @@
+import asyncio
 
+
+async def run_async(func, *args, **kwargs):
+    """
+    Run a blocking function asynchronously.
+    :param func: Blocking function
+    :param args: Function arguments
+    :param kwargs: Function keyword arguments
+    :return: Result of the function
+    """
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(None, func, *args, **kwargs)
+
+
+def safe_divide(a: float, b: float) -> float:
+    """
+    Safely divide two numbers without raising a ZeroDivisionError.
+    :param a: Numerator
+    :param b: Denominator
+    :return: Division result or 0 if denominator is 0
+    """
+    return a / b if b != 0 else 0
